d9212c0365337bd4ccfd28be477b4bb83154c417
Harry Lyu <lyuxiaochuan@gmail.com>
2024-04-12 19:46:26 -0400

    Finished the decode part, and fixed the logic and problems in the main function.

--- jes_lab9.py
+++ jes_lab9.py
@@ -1,5 +1,18 @@
 #Jessica Wu
-#partner: Harry Lyu
+#Partner: Harry Lyu
+
+def decode(password):
+    decoded_password_list = []
+    decoded_password_list[:] = password
+    digit = 0
+    decoded_password = ""
+    for i in range(0,len(decoded_password_list)):
+        if int(password[i]) < 3:
+            digit = int(decoded_password_list[i]) + 7
+        else:
+            digit = int(decoded_password_list[i]) - 3
+        decoded_password += str(digit)
+    return decoded_password
 
 def encode(password):
     encoded_password_list = []
@@ -7,9 +20,11 @@ def encode(password):
     digit = 0
     encoded_password = ""
     for i in range (len(encoded_password_list)):
-        digit = int(encoded_password_list[i]) + 3
+        if int(encoded_password_list[i]) > 6:
+            digit = int(encoded_password_list[i]) - 7
+        else:
+            digit = int(encoded_password_list[i]) + 3
         encoded_password += str(digit)
-
     return encoded_password
 
 def main():
@@ -22,17 +37,16 @@ def main():
               "3. Quit\n")
 
         option = input("Please enter an option: ")
-
         if option == "1":
-
             password = input("Please enter your password to encode: ")
-            encoded_password = encode(password)
-            print("Your password has been encoded and stored!")
-
-        if option == "3":
+            password = encode(password)
+            print("Your password has been encoded and stored!\n")
+        elif option == "2":
+            print(f"The encoded password is {password}, and the original password is {decode(password)}.\n")
+        elif option == "3":
             break
-
-
+        else:
+            print("Please enter valid choice number!\n")
 
 if __name__ == '__main__':
     main()
